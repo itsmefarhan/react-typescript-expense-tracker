7e34b6a5ec32b8f79cc405e3c20ec27d25c66575
Farhan Farooq <farhan.farooq@live.com>
2020-08-17 15:53:43 +0500

    push notifications functionality

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Expense Tracker with React, TypeScript and PWA
 
-This is project 7 of bootcamp. This project is built with React / TypeScript. It is also a Progressive Web App.
+This is project 7 of bootcamp. This project is built with React / TypeScript. It is also a Progressive Web App with Push Notifications capability.
 
 This app has passed automated tests before deployment. The app is deployed on [surge](https://react-ts-pwa-expense-tracker.surge.sh)
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-
+import firebase from "./firebase";
 import Balance from "./components/Balance";
 import AddTransaction from "./components/AddTransaction";
 import Transactions from "./components/Transactions";
-
 import TransactionContextProvider from "./context/TransactionContext";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const messaging = firebase.messaging();
+    messaging
+      .requestPermission()
+      .then(() => messaging.getToken())
+      .then((token) => console.log(token))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <TransactionContextProvider>
       <div className="container">
